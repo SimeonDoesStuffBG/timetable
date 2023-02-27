e4bfed02c63f70aa078198ff108023375d65f78f
SimeonDoesStuffBG <44085498+SimeonDoesStuffBG@users.noreply.github.com>
2023-02-28 00:36:18 +0200

    adsa

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "containers-us-west-95.railway.app",
-  port: "5885",
+  host: "containers-us-west-111.railway.app",
+  port: "5992",
   user: "root",
-  password: "jMJW6COyW71SeqCGurLZ",
+  password: "V9Y14gINY2suaD2LrfdI",
   database: "railway",
 });
 
