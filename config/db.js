dab435e96aa9a2e1fe7d67dad1f99c78a82d7b00
SimeonDoesStuffBG <44085498+SimeonDoesStuffBG@users.noreply.github.com>
2023-02-28 14:41:38 +0200

    db reconect

--- config/db.js
+++ config/db.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql");
 
-// const connection = mysql.createConnection({
-//   host: "containers-us-west-111.railway.app",
-//   port: "5992",
-//   user: "root",
-//   password: "V9Y14gINY2suaD2LrfdI",
-//   database: "railway",
-// });
-
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "containers-us-west-111.railway.app",
+  port: "5992",
   user: "root",
-  password: "",
-  database: "codeItprojectDB",
+  password: "V9Y14gINY2suaD2LrfdI",
+  database: "railway",
 });
+
+// const connection = mysql.createConnection({
+// host: "localhost",
+// user: "root",
+// password: "",
+// database: "codeItprojectDB",
+// });
 
 module.exports = connection;
