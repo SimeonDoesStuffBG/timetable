f2ced09949339aa124430ecfeec1cd86da054656
SimeonDoesStuffBG <44085498+SimeonDoesStuffBG@users.noreply.github.com>
2023-02-28 23:56:56 +0200

    db streamline

--- config/db.js
+++ config/db.js
@@ -1,25 +1,10 @@
 const mysql = require("mysql2");
 
-// const connection = mysql.createConnection({
-// host: "containers-us-west-95.railway.app",
-// port: "5885",
-// user: "root",
-// password: "jMJW6COyW71SeqCGurLZ",
-// database: "railway",
-// });
-
 const connection = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
 });
-
-// const connection = mysql.createConnection({
-// host: "localhost",
-// user: "root",
-// password: "",
-// database: "codeItprojectDB",
-// });
 
 module.exports = connection;
