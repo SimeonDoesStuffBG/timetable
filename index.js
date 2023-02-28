eaa0da421b9414a2950c956e51610384a3e5ca7b
SimeonDoesStuffBG <44085498+SimeonDoesStuffBG@users.noreply.github.com>
2023-02-28 14:35:45 +0200

    meep

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ db.connect((err) => {
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ encoded: false }));
-app.use("/timetable", require("./backend/routes/timetableRoutes"));
+app.use("/timetable", require("./routes/timetableRoutes"));
 
 app.get("/couriers", (req, res) => {
   const sql = "SELECT * FROM courier";
