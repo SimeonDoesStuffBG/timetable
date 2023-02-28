b2e7a214c24d6a463ff71e6009d8cd849335287b
SimeonDoesStuffBG <44085498+SimeonDoesStuffBG@users.noreply.github.com>
2023-02-28 15:17:47 +0200

    d

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "containers-us-west-111.railway.app",
-  port: "5992",
+  // port: "5992",
   user: "root",
   password: "V9Y14gINY2suaD2LrfdI",
   database: "railway",
