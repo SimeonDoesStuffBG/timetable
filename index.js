fd9768e590392ac3b48d6ab2307e88fa9533682c
SimeonDoesStuffBG <44085498+SimeonDoesStuffBG@users.noreply.github.com>
2023-03-01 07:25:32 +0200

    tester

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ app.get("/", (req, res) => {
   db.query(sql, (err, result) => {
     if (err) {
       console.log("meep");
+      console.log(err);
+      throw err;
     }
     console.log(result);
   });
