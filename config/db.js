c5436665f5d09a98a58280e077c0d170a9039a43
SimeonDoesStuffBG <44085498+SimeonDoesStuffBG@users.noreply.github.com>
2023-03-01 07:49:22 +0200

    db

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ const mysql = require("mysql2");
 
 const connection = mysql.createPool({
   host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
